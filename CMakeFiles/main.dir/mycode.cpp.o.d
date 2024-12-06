d7ce073dfaed81138ee36f4aff005fc9d6736ed4
Sagar <sagarsaren230696@gmail.com>
2024-12-06 22:04:50 +0900

    First commit

---
+++ CMakeFiles/main.dir/mycode.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/mycode.cpp.o: /home/ssaren/MED_CPP/mycode.cpp \
+ /usr/include/stdc-predef.h
---
+++ mycode.cpp
@@ -0,0 +1,8 @@
+// #include "CoolPropLib.h"
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << CoolProp::PropsSI("T","P",101325,"Q",0,"Water") << std::endl;
+//     return 1;
+// }
